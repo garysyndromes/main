c23212d03abf20a10cdd98d1407058848368b5b1
garysyndromes <garylimky@hotmail.com>
2020-03-03 23:48:26 +0800

    added remark command

--- src/main/java/seedu/address/logic/parser/AddressBookParser.java
+++ src/main/java/seedu/address/logic/parser/AddressBookParser.java
@@ -47,7 +47,7 @@ public class AddressBookParser {
 
         case AddCommand.COMMAND_WORD:
             return new AddCommandParser().parse(arguments);
-
+    
         case EditCommand.COMMAND_WORD:
             return new EditCommandParser().parse(arguments);
 
